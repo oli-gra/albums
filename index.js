b23d4426b66a72c1757ea388992bfa3f7b3ff153
oli-gra <oliver.grant@gmail.com>
2020-07-09 13:16:51 +0100

    header component

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-/**
- * @format
- */
+import React from 'react'
+import { AppRegistry } from 'react-native'
+import Header from './src/components/header'
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const App = () => <Header text="Albums" />
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('albums', () => App)
